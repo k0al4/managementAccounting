e562700f63230ea6f1444f757b529e239f9bcf86
Denis Lima e Alves, PhD <39866187+k0al4@users.noreply.github.com>
2025-01-30 11:21:09 +0000

    Create ex_1.py

---
+++ ex_1.py
@@ -0,0 +1,22 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+from ipywidgets import interactive, Select, interact
+
+sns.set()
+
+tips = sns.load_dataset('tips')
+
+plt.figure(figsize=(10, 6))
+
+sex_options = tips['sex'].unique()
+time_options = tips.time.unique().tolist()
+smoker_options = tips.smoker.unique().tolist()
+
+def update_chart(smoker, time):
+    sns.lineplot(x='total_bill', y='tip', hue='day',
+                 data=tips[(tips['smoker'] == smoker) & (tips['time'] == time)])
+    plt.show()
+
+interact(update_chart,
+         smoker=Select(options=smoker_options),
+         time=Select(options=time_options));
